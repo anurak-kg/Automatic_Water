8a687932ea150598b2e9d64411ac0591f402f03d
kurodo99 <imannn.99@gmail.com>
2016-03-17 04:22:03 +0700

    add static

--- Class/Statistic.py
+++ Class/Statistic.py
@@ -16,6 +16,7 @@ class Statistic:
     def update_data(self):
         # Water Level
         self.current_status[RedisDatabase.WATER_LEVEL] = self.redis_database.get_water_level()
+
         # Screen Mode
         if self.redis_database.get_screen_mode() is not None:
             self.current_status[RedisDatabase.SCREEN_MODE] = self.redis_database.get_screen_mode()
@@ -24,6 +25,10 @@ class Statistic:
 
         # Date
         self.current_status["date"] = datetime.datetime.utcnow()
+
+        # TEMPERATURE and HUMIDITY
+        self.current_status[RedisDatabase.HUMIDITY] = float(self.redis_database.get(RedisDatabase.HUMIDITY))
+        self.current_status[RedisDatabase.TEMPERATURE] = float(self.redis_database.get(RedisDatabase.TEMPERATURE))
 
     def save(self):
         self.stat.insert_one(self.current_status)
