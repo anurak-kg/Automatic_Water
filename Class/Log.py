d5b75f9f075513bc18ffe519de87f245586fe509
kurodo99 <imannn.99@gmail.com>
2016-03-24 04:41:50 +0700

    refactor Log class

--- Class/Log.py
+++ Class/Log.py
@@ -27,3 +27,26 @@ class Log:
         except Exception, e:
             print(e)
             print("Error save logs")
+
+    @staticmethod
+    def debug(description):
+        Log.insert(Log.DEBUG, description)
+
+    @staticmethod
+    def insert(type_error, description):
+        try:
+            description = description.encode('utf-8')
+            mongo_client = MongoClient()
+            mongo_database = mongo_client["smart_aqua"]
+            log = mongo_database.logs
+            log_data = {
+                "type": type_error,
+                "description": description,
+                "date": datetime.datetime.utcnow()
+            }
+            log.insert_one(log_data)
+            print("Log: " + str(description))
+
+        except Exception, e:
+            print(e)
+            print("Error save logs")
